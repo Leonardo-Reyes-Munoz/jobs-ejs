7062169498d03343922161507495c87e05dd8754
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2024-01-14 10:40:57 -0800

    feat: add success messages for add, delete, and update job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,9 +1,10 @@
 const Job = require('../models/Job');
 const { StatusCodes } = require('http-status-codes');
+const parseVErr = require('../utils/parseValidationErr');
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.id }).sort('CreatedAt');
-  res.render('jobs', { jobs });
+  res.status(StatusCodes.OK).render('jobs', { jobs });
 };
 
 const addJob = async (req, res) => {
@@ -12,7 +13,8 @@ const addJob = async (req, res) => {
   res.status(StatusCodes.CREATED);
 
   const jobs = await Job.find({ createdBy: req.user.id }).sort('CreatedAt');
-  res.render('jobs', { jobs });
+  const info = [`Job has been successfully added.`];
+  res.render('jobs', { jobs, info });
 };
 
 const addForm = async (req, res) => {
@@ -32,11 +34,53 @@ const editForm = async (req, res) => {
 };
 
 const editJob = async (req, res) => {
-  res.send('Display Edit Job');
+  const {
+    body: { company, position, status },
+    user: { id: userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === '' || position === '') {
+    throw new Error('Company of position fields cannot be empty');
+  }
+
+  const job = await Job.findOneAndUpdate(
+    {
+      _id: jobId,
+      createdBy: userId,
+    },
+    req.body,
+    { new: true, runValidators: true }
+  );
+  const info = [`Job has been successfully updated.`];
+
+  if (!job) {
+    throw new Error(`No job with id ${jobId} found.`);
+  }
+
+  const jobs = await Job.find({ createdBy: userId }).sort('CreatedAt');
+  res.render('jobs', { jobs, info });
 };
 
 const deleteJob = async (req, res) => {
-  res.send('Delete Job');
+  const {
+    user: { id: userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndDelete({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  const info = [`Job has been successfully deleted.`];
+
+  if (!job) {
+    throw new Error(`No job with id ${jobId} found`);
+  }
+
+  const jobs = await Job.find({ createdBy: userId }).sort('CreatedAt');
+  res.render('jobs', { jobs, info });
 };
 
 module.exports = {
